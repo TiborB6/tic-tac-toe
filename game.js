482d17490be0ca89cd99c25962ad8609e2f74f0a
Tibor Balogh <btibor@gmx.at>
2023-06-08 22:32:53 +0200

    Player vs Player
    Winning logic is build out player vs player is fully playable
    Animations and Computer Logic is missing
    no known bugs

--- game.js
+++ game.js
@@ -31,6 +31,7 @@ function createPlayer(color, turn) {
     return {
         color: color,
         turn: turn,
+        winArr: [],
         playerChoice: function(btnId) {
             const choice = document.querySelector(`#${btnId}`);
             if (choice.innerHTML === "") {
@@ -44,6 +45,7 @@ function createComputer(color, turn) {
     return {
         color: color,
         turn: turn,
+        winArr: [],
         computerChoice: () => {
             // Needs to be build out
             // Logic so computer chooses perfect choice
@@ -51,6 +53,7 @@ function createComputer(color, turn) {
     };
 }
 
+//Create first to player
 let playerOne = createPlayer("X", 1);
 let playerTwo = createPlayer("O", 0);
 
@@ -71,18 +74,63 @@ const gameLogicModule = (function() {
             console.log("Turn ERROR");
         }
 
-        function checkWin() {
-            // Implementation of win checking logic
-            // !!! needs to be build out
-        }
+        // Checks for win by getting all elements that have a choice inside them and adds them to the player
+        // Then checks this arrays if there is a winning condition 
+        // If there is a highlight is added to the winning team
+        function checkWin(playerOne, playerTwo) {
+            const checked = document.querySelectorAll(".choice > p")
+            checked.forEach(check => {
+                if (check.textContent == "X") {
+                    playerOne.winArr.push(check.parentNode.id);
+                }
+                if (check.textContent == "O") {
+                    playerTwo.winArr.push(check.parentNode.id);
+                } 
+            })
 
+            function winEvent(player){
+                player.winArr.forEach(choice => {
+                    const winChoice = document.querySelector(`#${choice}`);
+                    winChoice.classList.add("highlight-winner");
+                })
+            }
+
+            if (
+                playerOne.winArr.includes("choice1") && playerOne.winArr.includes("choice2") && playerOne.winArr.includes("choice3") ||
+                playerOne.winArr.includes("choice4") && playerOne.winArr.includes("choice5") && playerOne.winArr.includes("choice6") ||
+                playerOne.winArr.includes("choice7") && playerOne.winArr.includes("choice8") && playerOne.winArr.includes("choice9") ||
+                playerOne.winArr.includes("choice1") && playerOne.winArr.includes("choice4") && playerOne.winArr.includes("choice7") ||
+                playerOne.winArr.includes("choice2") && playerOne.winArr.includes("choice5") && playerOne.winArr.includes("choice8") ||
+                playerOne.winArr.includes("choice3") && playerOne.winArr.includes("choice6") && playerOne.winArr.includes("choice9") ||
+                playerOne.winArr.includes("choice1") && playerOne.winArr.includes("choice5") && playerOne.winArr.includes("choice9") ||
+                playerOne.winArr.includes("choice3") && playerOne.winArr.includes("choice5") && playerOne.winArr.includes("choice7") 
+            ){
+                winEvent(playerOne);
+            } else if (
+                playerTwo.winArr.includes("choice1") && playerTwo.winArr.includes("choice2") && playerTwo.winArr.includes("choice3") ||
+                playerTwo.winArr.includes("choice4") && playerTwo.winArr.includes("choice5") && playerTwo.winArr.includes("choice6") ||
+                playerTwo.winArr.includes("choice7") && playerTwo.winArr.includes("choice8") && playerTwo.winArr.includes("choice9") ||
+                playerTwo.winArr.includes("choice1") && playerTwo.winArr.includes("choice4") && playerTwo.winArr.includes("choice7") ||
+                playerTwo.winArr.includes("choice2") && playerTwo.winArr.includes("choice5") && playerTwo.winArr.includes("choice8") ||
+                playerTwo.winArr.includes("choice3") && playerTwo.winArr.includes("choice6") && playerTwo.winArr.includes("choice9") ||
+                playerTwo.winArr.includes("choice1") && playerTwo.winArr.includes("choice5") && playerTwo.winArr.includes("choice9") ||
+                playerTwo.winArr.includes("choice3") && playerTwo.winArr.includes("choice5") && playerTwo.winArr.includes("choice7") 
+            ) {
+                winEvent(playerTwo)
+            } else {
+                playTurn(playerOne, playerTwo); //Intiates new turn
+            };
+
+            playerOne.winArr = [];
+            playerTwo.winArr = [];
+        }   
         // Player can set choices
         function handleEvent(event) {
             player.playerChoice(event.target.id); //Sets X or O 
             player.turn = 0; 
             notPlayer.turn = 1; // Changes turns
-            playTurn(playerOne, playerTwo); //Intiates new turn
             deleteEvents();//deletes all event listeners so there is no collision
+            checkWin(playerOne, playerTwo);
         }
         const choiceFields = document.querySelectorAll(".choice");
         choiceFields.forEach(choiceField => {
@@ -104,7 +152,14 @@ const gameLogicModule = (function() {
 
 gameLogicModule.playTurn(playerOne, playerTwo);
 
+// Handels the reset
 const resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", () => {
     gameModule.resetGamefield();
-})
+    gameLogicModule.playTurn(playerOne, playerTwo);
+
+    const highlights = document.querySelectorAll(".highlight-winner");
+    highlights.forEach(highlight => {
+        highlight.classList.remove("highlight-winner");
+    });    
+});
